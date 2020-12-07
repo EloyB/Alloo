667a4f6acd8233cbf44405af514d018567e47377
Eloy Boone <eloyboone@hotmail.com>
2020-12-07 20:20:40 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from "expo-camera";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { Button, Platform, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [toggleFlash, setToggleFlash] = useState(false);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {toggleFlash && (
+        <Camera
+          style={{
+            width: 1,
+            height: 1,
+            alignSelf: "stretch",
+          }}
+          type={type}
+          flashMode="torch"
+        />
+      )}
+
+      <Button
+        onPress={() => setToggleFlash(!toggleFlash)}
+        title="Turn On"
+        color="#841584"
+      />
     </View>
   );
 }
@@ -14,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
